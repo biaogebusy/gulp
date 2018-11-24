b0c5a7f9165d4d76a4c499f12431e057c76658f5
biaogebusy <349255833@qq.com>
2018-11-24 11:29:31 +0800

    images mim

--- gulpfile.js
+++ gulpfile.js
@@ -11,10 +11,10 @@ var imagemin = require('gulp-imagemin'); // 图片压缩
 /**
  * png图片量化， 基于我们的视觉和大脑任务非常相似的颜色，
  * 并重新映射为新的优化颜色， 如果没有一定的质量， 不会保存
- */ 
+ */
 var pngquant = require('imagemin-pngquant');
 
-gulp.task('default',['styles', 'copy-images', 'copy-html'], function(){
+gulp.task('default', ['styles', 'copy-images', 'copy-html'], function () {
 	gulp.watch('sass/**/*.scss', ['styles']);
 	gulp.watch('/index.html', ['copy-html']);
 });
@@ -29,13 +29,13 @@ gulp.task('dist', [
 	'scripts-dist'
 ]);
 
-gulp.task('scripts', function(){
+gulp.task('scripts', function () {
 	gulp.src('js/**/*.js')
-			.pipe(babel({
-				presets: ['@babel/env']
-			}))
-			.pipe(concat('all.js'))
-			.pipe(gulp.dest('dist/js'));
+		.pipe(babel({
+			presets: ['@babel/env']
+		}))
+		.pipe(concat('all.js'))
+		.pipe(gulp.dest('dist/js'));
 })
 
 gulp.task('scripts-dist', function () {
@@ -53,13 +53,29 @@ gulp.task('scripts-dist', function () {
 /**
  * 复制图片
  */
-gulp.task('copy-images', function(){
+gulp.task('copy-images', function () {
 	gulp.src('images/*')
-			.pipe(imagemin({
-				progressive: true,
-				use: [pngquant()]
-			}))
-			.pipe(gulp.dest('dist/img'));
+		.pipe(imagemin([
+			imagemin.gifsicle({
+				interlaced: true
+			}),
+			imagemin.jpegtran({
+				progressive: true
+			}),
+			imagemin.optipng({
+				optimizationLevel: 5
+			}),
+			imagemin.svgo({
+				plugins: [{
+						removeViewBox: true
+					},
+					{
+						cleanupIDs: false
+					}
+				]
+			})
+		]))
+		.pipe(gulp.dest('dist/img'));
 });
 
 /**
@@ -73,20 +89,20 @@ gulp.task('copy-html', function () {
 /**
  * 新建一个styles的任务，并执行第二个参数的匿名函数
  */
-gulp.task('styles', function(){
+gulp.task('styles', function () {
 	// 获取源文件
 	gulp.src('sass/**/*.scss')
-			// 有了这些文件就可以加入管道，使用sass进行处理，并转换成css
-			// 当sass源文件写法错误时，监听错误并输出，并不冲断构建，体验更好
-			.pipe(sass({
-				outputStyle: 'compressed'
-			}).on('error', sass.logError))
-			// 在css生成之前，进行添加前缀处理，并指定浏览器
-			.pipe(autoprefixer({
-				browsers: ['last 2 versions']
-			}))
-			// 再使用gulp.dest()把生成的文件放到./css目录
-	    .pipe(gulp.dest('dist/css/'));
+		// 有了这些文件就可以加入管道，使用sass进行处理，并转换成css
+		// 当sass源文件写法错误时，监听错误并输出，并不冲断构建，体验更好
+		.pipe(sass({
+			outputStyle: 'compressed'
+		}).on('error', sass.logError))
+		// 在css生成之前，进行添加前缀处理，并指定浏览器
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions']
+		}))
+		// 再使用gulp.dest()把生成的文件放到./css目录
+		.pipe(gulp.dest('dist/css/'));
 });
 
 /**
